6fbbc916e4bd258d427973c727a1bae0a6daa0fb
Luis Segoviano <the.segoviano@gmail.com>
2022-02-26 14:23:01 -0600

    Initial Commit

---
+++ CropVideoUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CropVideoUI
+//
+//  Created by Luis Segoviano on 26/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
